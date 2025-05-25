2b331166c7c2d4a783b4f648a22cfe07f96872f4
HLDDian <a59715a@yahoo.com.tw>
2025-05-25 23:33:05 +0800

    新增 PrimeReact 和 PrimeIcons 依賴，更新 CSS 引入方式，並在佈局中加入 NavBar 元件。

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import NavBar from "@/components/NavBar";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +32,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <NavBar />
         {children}
       </body>
     </html>
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { Menubar } from "primereact/menubar";
+import { Button } from "primereact/button";
+import { useRouter } from "next/navigation";
+
+export default function NavBar() {
+    const router = useRouter();
+    const items = [
+        {
+            label: "首頁 Home",
+            icon: "pi pi-home",
+            command: () => router.push("/"),
+        },
+        {
+            label: "多層選單",
+            icon: "pi pi-check",
+            items: [
+                {
+                    label: "頁面一",
+                    icon: "pi pi-plus",
+                    command: () => router.push("/page1"),
+                },
+                {
+                    label: "頁面二",
+                    icon: "pi pi-plus",
+                    command: () => router.push("/page2"),
+                },
+
+            ],
+        },
+    ];
+    const end = (
+        <div className="flex align-items-center gap-2">
+            <Button icon="pi pi-user" rounded text />
+        </div>
+    );
+
+    return (
+        <div className="card">
+            <Menubar model={items} end={end} />
+        </div>
+    );
+}
